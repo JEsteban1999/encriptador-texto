5b10c980f3a426553c89bb95eebe17a6d23ea188
Juan Esteban Narváez Carvajal <116776212+JEsteban1999@users.noreply.github.com>
2024-08-15 11:24:18 -0500

    Arreglo validacion letras minusculas

--- js/app.js
+++ js/app.js
@@ -1,39 +1,101 @@
+// function encriptar() {
+//     let shift = 3;
+//     let text = document.querySelector(".programa_entrada_texto").value;
+//     let result = ''; // Cadena para almacenar el texto cifrado
+//     for (let i = 0; i < text.length; i++) {
+//         let char = text[i];
+//         if (char.match(/[a-z]/)) { // Verifica si el carácter es una letra minúscula
+//             let code = text.charCodeAt(i); // Obtiene el valor ASCII del carácter
+//             // Maneja las letras minúsculas
+//             char = String.fromCharCode(((code - 97 + shift) % 26) + 97);
+//         }
+//         result += char; // Añade el carácter cifrado (o sin cambios) al resultado
+//     }
+//     let salida = document.querySelector(".programa_salida");
+//     salida.innerHTML = `
+//         <div class="contenedor_texto_botoncopiar">
+//             <p id="resultado_texto">${result}</p>
+//             <button class="boton_copiar" onclick="copiar()" type="button">Copiar</button>
+//         </div>
+//     `;
+// }
+
 function encriptar() {
-    let shift = 3;
     let text = document.querySelector(".programa_entrada_texto").value;
     let result = ''; // Cadena para almacenar el texto cifrado
     for (let i = 0; i < text.length; i++) {
         let char = text[i];
-        console.log(char);
-        if (char.match(/[a-z]/)) { // Verifica si el carácter es una letra minúscula
-            let code = text.charCodeAt(i); // Obtiene el valor ASCII del carácter
-            // Maneja las letras minúsculas
-            char = String.fromCharCode(((code - 97 + shift) % 26) + 97);
+        if (char.match((/[a-z]/))) {
+            switch (char) {
+                case 'a':
+                    char = 'ai';
+                    break;
+                case 'e':
+                    char = 'enter';
+                    break;
+                case 'i':
+                    char = 'imes';
+                    break;
+                case 'o':
+                    char = 'ober';
+                    break;
+                case 'u':
+                    char = 'ufat';
+                    break;
+                default:
+                    break;
+            }
+            result += char; // Añade el carácter cifrado (o sin cambios) al resultado
+        } else {
+            alert('Recuerda escribir solo letras en minúscula');
+            return;
         }
-        result += char; // Añade el carácter cifrado (o sin cambios) al resultado
     }
     let salida = document.querySelector(".programa_salida");
     salida.innerHTML = `
-        <div class="contenedor_texto_botoncopiar">
-            <p id="resultado_texto">${result}</p>
-            <button class="boton_copiar" onclick="copiar()" type="button">Copiar</button>
-        </div>
-    `;
+            <div class="contenedor_texto_botoncopiar">
+                <p id="resultado_texto">${result}</p>
+                <button class="boton_copiar" onclick="copiar()" type="button">Copiar</button>
+            </div>
+        `;
 }
 
+// function desencriptar() {
+//     let shift = 3;
+//     let text = document.querySelector(".programa_entrada_texto").value;
+//     let result = ''; // Cadena para almacenar el texto descifrado
+//     for (let i = 0; i < text.length; i++) {
+//         let char = text[i];
+//         if (char.match(/[a-z]/)) { // Verifica si el carácter es una letra minúscula
+//             let code = text.charCodeAt(i); // Obtiene el valor ASCII del carácter
+//             // Maneja las letras minúsculas
+//             char = String.fromCharCode(((code - 97 - shift + 26) % 26) + 97);
+//         }
+//         result += char; // Añade el carácter descifrado (o sin cambios) al resultado
+//     }
+//     let salida = document.querySelector(".programa_salida");
+//     salida.innerHTML = `
+//         <div class="contenedor_texto_botoncopiar">
+//             <p id="resultado_texto">${result}</p>
+//             <button class="boton_copiar" onclick="copiar()" type="button">Copiar</button>
+//         </div>
+//     `;
+// }
+
 function desencriptar() {
-    let shift = 3;
     let text = document.querySelector(".programa_entrada_texto").value;
-    let result = ''; // Cadena para almacenar el texto descifrado
     for (let i = 0; i < text.length; i++) {
         let char = text[i];
-        if (char.match(/[a-z]/)) { // Verifica si el carácter es una letra minúscula
-            let code = text.charCodeAt(i); // Obtiene el valor ASCII del carácter
-            // Maneja las letras minúsculas
-            char = String.fromCharCode(((code - 97 - shift + 26) % 26) + 97);
+        if (!char.match((/[a-z]/))) {
+            alert('Recuerda escribir solo letras en minúscula');
+            return;
         }
-        result += char; // Añade el carácter descifrado (o sin cambios) al resultado
     }
+    let result = text.replace(/ai/g, 'a')
+        .replace(/enter/g, 'e')
+        .replace(/imes/g, 'i')
+        .replace(/ober/g, 'o')
+        .replace(/ufat/g, 'u');
     let salida = document.querySelector(".programa_salida");
     salida.innerHTML = `
         <div class="contenedor_texto_botoncopiar">
@@ -46,14 +108,6 @@ function desencriptar() {
 function copiar() {
     const textToCopy = document.querySelector("#resultado_texto").innerHTML;
     navigator.clipboard.writeText(textToCopy);
+    // Limpiar la entrada de texto
+    document.querySelector(".programa_entrada_texto").value = '';
 }
-
-//const shift = 3; // Número de posiciones a desplazar
-
-// // Encriptar
-// const encryptedText = encryptCaesarCipher(originalText, shift);
-// console.log('Encrypted:', encryptedText); // Heoor, Worog!
-
-// // Desencriptar
-// const decryptedText = decryptCaesarCipher(encryptedText, shift);
-// console.log('Decrypted:', decryptedText); // Hello, World!
